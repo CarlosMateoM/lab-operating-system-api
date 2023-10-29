ba25a9aab411bb7ba919f352d5f45763b4025e1b
CarlosMateoM <carlosmateo484@gmail.com>
2023-10-28 21:27:05 -0500

    Change: NotFoundHttp and MethodNotAllowed exception responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +27,21 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status_code' => 404,
+                    'message' => 'not found'
+                ], 404);
+            }
+        });
+
+        $this->renderable(function (MethodNotAllowedHttpException $e, Request $request) {
+            return response()->json([
+                'status_code' => 405,
+                'message' => 'unexpected http method'
+            ], 405);
         });
     }
 }
